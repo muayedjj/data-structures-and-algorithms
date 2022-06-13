0300b06cbcfbd5a22f20ba8e6d20eef1f56f919e
Mu'ayad Al Shareef <103497877+muayedjj@users.noreply.github.com>
2022-06-13 23:56:36 +0300

    ins_2.1

--- linked_list/README.md
+++ linked_list/README.md
@@ -11,33 +11,18 @@ Create a Node class that has properties for the value stored in the Node, and a 
 - Create a Linked List class
 - Within your Linked List class, include a head property.
 - Upon instantiation, an empty Linked List should be created.
-- The class should contain the following methods
-
-1. _insert_
-        Arguments: value
-        Returns: nothing
-        Adds a new node with that value to the head of the list with an O(1) Time performance.
-
-2. _includes_
-   - Arguments: value
-   - Returns: Boolean
-   - Indicates whether that value exists as a Node’s value somewhere within the list.
-
-3. _to string_
-   - Arguments: none
-   - Returns: a string representing all the values in the Linked List, formatted as:
-   `{ a } -> { b } -> { c } -> NULL`
-   
-- Any exceptions or errors that come from your code should be semantic, capture-able errors. For example, rather than a default error thrown by your language, your code should raise/throw a custom, semantic error that describes what went wrong in calling the methods you wrote for this lab.
-   Be sure to follow your language/frameworks standard naming conventions 
 
 
+## Whiteboard Process
 
-### The program should be able to do the following:
-- Can successfully instantiate an empty linked list
-- Can properly insert into the linked list
-- The head property will properly point to the first node in the linked list
-- Can properly insert multiple nodes into the linked list
-- Will return true when finding a value within the linked list that exists
-- Will return false when searching for a value in the linked list that does not exist
-- Can properly return a collection of all the values that exist in the linked list
+[]()
+
+## Approach & Efficiency
+- Single responsibility principle
+- 
+- 
+
+## Solution
+[code](./linked_list.py)
+
+[test](../tests/test_linked_list.py)
--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -2,6 +2,7 @@ class Node:
     """
     Node creator
     """
+
     def __init__(self, value, next=None):
         self.value = value
         self.next = next
@@ -11,10 +12,15 @@ class Linked_List:
     """
     Node Navigator and Modifier
     """
+
     def __init__(self, head=None):
         self.head = head
 
     def includes(self, value):
+        """
+         Node finder
+        """
+
         current = self.head
         while current is not None:
             if value == current.value:
@@ -23,11 +29,13 @@ class Linked_List:
         return False
 
     def insert(self, value):
-        node = Node(value)
+        """
+         Node pusher
+        """
 
+        node = Node(value)
         if self.head is not None:
             node.next = self.head
-
         self.head = node
 
     def __str__(self):
@@ -37,3 +45,51 @@ class Linked_List:
             string += "{ " + f"{current.value}" + " }" + " -> "
             current = current.next
         return string + "NULL"
+
+    def append(self, value):
+        """
+         Node adder
+        """
+
+        node = Node(value)
+        current = self.head
+        if self.head is None:
+            self.head = node
+            return
+        while current.next is not None:
+            current = current.next
+        current.next = node
+
+    def insert_before(self, value, new_value):
+        """
+         Node inserter
+        """
+
+        current = self.head
+        if current.value == value:
+            self.insert(new_value)
+            return
+        while current is not None:
+            if current.next.value == value:
+                node = Node(new_value)
+                node.next = current.next
+                current.next = node
+                return
+            current = current.next
+        raise Exception(f"{{value}} Node non-existent, sorry")
+
+    def insert_after(self, value, new_value):
+        """
+         Node inserter
+        """
+
+        current = self.head
+        while current is not None:
+            if current.value is value:
+                node = Node(new_value)
+                node.next = current.next
+                current.next = node
+                return
+            current = current.next
+
+        raise Exception(f"{{value}} Node non-existent, sorry")

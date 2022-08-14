bb3bc68da94ad2382a694f48a177ed90b1af1923
Mu'ayad Al Shareef <103497877+muayedjj@users.noreply.github.com>
2022-08-15 00:44:11 +0300

    Feat: Code Implemnentation

---
+++ data_structures_py/hash_tables/hashmap_left_join.py
@@ -0,0 +1,46 @@
+from data_structures_py.hash_tables.hash_table import HashTable
+
+
+def left_join(ht1, ht2):
+    output = []
+    k = 0
+    for g in ht1.key():
+        k += 1
+        if ht2.contains(g):
+            output.append([g, ht1.get(g), ht2.get(g)])
+
+        else:
+            output.append([g, ht1.get(g), 'Null'])
+
+    return output
+
+
+if __name__ == "__main__":
+
+    ht1 = HashTable()
+    ht2 = HashTable()
+
+    arr1 = [
+        ["font", "enamored"],
+        ["wrath", "anger"],
+        ["diligent", "employed"],
+        ["outfit", "garb"],
+        ["guide", "usher"]
+        ]
+
+    arr2 = [
+        ["font", "averse"],
+        ["wrath", "delight"],
+        ["diligent", "idle"],
+        ["flow", "jam"],
+        ["guide", "follow"]
+    ]
+
+    for i in range(len(arr1)):
+
+        ht1.set(arr1[i][0], arr1[i][1])
+        ht2.set(arr2[i][0], arr2[i][1])
+
+    print(left_join(ht1, ht2))
+
+    # print(ht1.key())

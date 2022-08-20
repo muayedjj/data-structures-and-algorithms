3afcaf047efe25daa96aecd42ae32fdb32a3c21f
Mu'ayad Al Shareef <103497877+muayedjj@users.noreply.github.com>
2022-08-21 01:10:03 +0300

    for future reference

--- data_structures_py/graphs/graphs.py
+++ data_structures_py/graphs/graphs.py
@@ -78,3 +78,45 @@ class Graph:
 
         else:
             return self.__repr__()
+
+
+
+    #
+    # def breadth_first(self, start_vertex):
+    #     result = []
+    #     visited = set()
+    #     q = Queue()
+    #
+    #     q.enqueue(start_vertex)
+    #     visited.add(start_vertex)
+    #
+    #     while len(q):
+    #         current_vertex = q.dequeue()
+    #
+    #         result.append(current_vertex.value)
+    #
+    #         neighbors = self.get_neighbors(current_vertex.value)
+    #
+    #         for edge in neighbors:
+    #             neighbor = edge.vertex
+    #             if neighbor not in visited:
+    #                 q.enqueue(neighbor)
+    #                 visited.add(neighbor)
+    #
+    #     return result
+
+if __name__ == "__main__":
+    g = Graph()
+    a = g.add_node('A')
+    b = g.add_node('B')
+    e = g.add_node('E')
+    c = g.add_node('C')
+    d = g.add_node('D')
+    g.add_edge(a, b, 1)
+    g.add_edge(a, c, 2)
+    g.add_edge(a, d, 3)
+    g.add_edge(a, e, 4)
+    g.add_edge(c, d, 5)
+    g.add_edge(d, e)
+
+    print(g)
